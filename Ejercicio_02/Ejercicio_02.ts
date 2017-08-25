f67ea826d8858cf0655f95d0d0f74c5177949765
1caruxx <fernandohugolareu@hotmail.com>
2017-08-24 22:20:21 -0300

    gfgfg
    jhhgjhjh

---
+++ Ejercicio_02/Ejercicio_02.ts
@@ -0,0 +1,6 @@
+var meses : string[] = ["Enero", "Febrero", "Marzo", "Abril", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+for(var i : number=0 ; i<12 ; i++)
+    {
+        console.log("0" + (i+1) + " "+ meses[i]);
+    }
