1ff295f0f62a0729b6696fb0ca9f2e556e8640e7
1caruxx <fernandohugolareu@hotmail.com>
2017-08-27 12:40:16 -0300

    gfgfg
    kjkjj

---
+++ B. Ejercicios de HTML/html_ts/main1.js
@@ -0,0 +1,12 @@
+function Click() {
+    alert("desde funcion...");
+}
+var Manejadora = (function () {
+    function Manejadora() {
+    }
+    Manejadora.Click = function () {
+        alert("desde clase...");
+    };
+    return Manejadora;
+}());
+//# sourceMappingURL=main1.js.map
---
+++ B. Ejercicios de HTML/html_ts/main2.js
@@ -0,0 +1,5 @@
+function Saludar() {
+    var nombre = document.getElementById("txtNombre").value;
+    alert("Hola " + nombre);
+}
+//# sourceMappingURL=main2.js.map
